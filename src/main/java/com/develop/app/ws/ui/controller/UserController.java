2eb86747c31d26f657dae0a1cc623e49661d979a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-07-17 00:32:50 +0300

    Create request model for a new user

--- src/main/java/com/develop/app/ws/ui/controller/UserController.java
+++ src/main/java/com/develop/app/ws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.develop.app.ws.ui.controller;
 
+import com.develop.app.ws.ui.model.request.UserDetailsRequestModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
