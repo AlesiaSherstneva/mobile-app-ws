ab9440ff909f22d2703fe2e0bbb08b407b24f1c5
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-07-19 22:54:33 +0300

    Create user DTO class

---
+++ src/main/java/com/develop/app/ws/shared/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.develop.app.ws.shared.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class UserDto implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1282136208263738692L;
+
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String encryptedPassword;
+    private String emailVerificationToken;
+    private Boolean emailVerificationStatus;
+}
