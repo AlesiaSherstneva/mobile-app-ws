14152a6125a8196c5514ceb3a8b541996ba4a537
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-07-26 23:46:52 +0300

    Create class for user entity

---
+++ src/main/java/com/develop/app/ws/io/entity/UserEntity.java
@@ -0,0 +1,46 @@
+package com.develop.app.ws.io.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class UserEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 7101931848323927786L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, length = 120)
+    private String email;
+
+    @Column(nullable = false)
+    private String encryptedPassword;
+
+    @Column
+    private String emailVerificationToken;
+
+    @Column(nullable = false)
+    private Boolean emailVerificationStatus = false;
+}
