97821c697235efd4689fe54c827ab43070404e5d
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-07-28 00:52:10 +0300

    Inject user repository into the service

--- src/main/java/com/develop/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/develop/app/ws/service/impl/UserServiceImpl.java
@@ -1,13 +1,31 @@
 package com.develop.app.ws.service.impl;
 
+import com.develop.app.ws.io.entity.UserEntity;
+import com.develop.app.ws.repository.UserRepository;
 import com.develop.app.ws.service.UserService;
 import com.develop.app.ws.shared.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+
+        return returnValue;
     }
 }
