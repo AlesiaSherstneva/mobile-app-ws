c02a8857a046ff7e2752a195ee7181c665e14246
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-07-29 01:29:32 +0300

    Fix the bug with user id copying

--- src/main/java/com/develop/app/ws/io/entity/UserEntity.java
+++ src/main/java/com/develop/app/ws/io/entity/UserEntity.java
@@ -21,7 +21,7 @@ public class UserEntity implements Serializable {
 
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String userId;
