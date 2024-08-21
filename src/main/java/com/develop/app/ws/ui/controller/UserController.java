393767d127644654412072c6f993f0959a7b2e2c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-08-21 21:44:44 +0300

    Make GET method consuming XML or JSON both, JSON first by default

--- src/main/java/com/develop/app/ws/ui/controller/UserController.java
+++ src/main/java/com/develop/app/ws/ui/controller/UserController.java
@@ -23,7 +23,9 @@ public class UserController {
     private final UserService userService;
 
     // @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_XML_VALUE)
-    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public UserResponseModel getUser(@PathVariable String id) {
         UserResponseModel response = new UserResponseModel();
 
