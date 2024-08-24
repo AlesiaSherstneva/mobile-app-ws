17d536f5a0c7b84b86c5bf45ddcae60dd5ca3f4e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-08-24 14:04:19 +0300

    Customize error message representation

--- src/main/java/com/develop/app/ws/exception/AppExceptionsHandler.java
+++ src/main/java/com/develop/app/ws/exception/AppExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.develop.app.ws.exception;
 
+import com.develop.app.ws.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,11 +8,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.time.Instant;
+
 @ControllerAdvice
 public class AppExceptionsHandler {
     @ExceptionHandler(value = UserServiceException.class)
     public ResponseEntity<Object> handleUserServiceException(UserServiceException exception,
                                                              WebRequest request) {
-        return new ResponseEntity<>(exception.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorMessage errorMessage = new ErrorMessage(Instant.now(), exception.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
---
+++ src/main/java/com/develop/app/ws/ui/model/response/ErrorMessage.java
@@ -0,0 +1,17 @@
+package com.develop.app.ws.ui.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+    private Instant timestamp;
+    private String message;
+}
