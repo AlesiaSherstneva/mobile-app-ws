2a9a7f47df6faa7bb664794344682af54921c2a9
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-08-25 14:22:46 +0300

    Add method for handling all other exceptions

--- src/main/java/com/develop/app/ws/exception/AppExceptionsHandler.java
+++ src/main/java/com/develop/app/ws/exception/AppExceptionsHandler.java
@@ -6,15 +6,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.time.Instant;
 
 @ControllerAdvice
 public class AppExceptionsHandler {
     @ExceptionHandler(value = UserServiceException.class)
-    public ResponseEntity<Object> handleUserServiceException(UserServiceException exception,
-                                                             WebRequest request) {
+    public ResponseEntity<Object> handleUserServiceException(UserServiceException exception) {
+        ErrorMessage errorMessage = new ErrorMessage(Instant.now(), exception.getMessage());
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Object> handleOtherException(Exception exception) {
         ErrorMessage errorMessage = new ErrorMessage(Instant.now(), exception.getMessage());
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
