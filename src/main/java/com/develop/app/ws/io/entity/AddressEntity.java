d4d0453522fe9681e94d2c367b62a2802b4756f4
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-09-18 01:18:02 +0300

    Create class for address entity

---
+++ src/main/java/com/develop/app/ws/io/entity/AddressEntity.java
@@ -0,0 +1,51 @@
+package com.develop.app.ws.io.entity;
+
+import com.develop.app.ws.shared.dto.UserDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+public class AddressEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -4626507017462769252L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String addressId;
+
+    @Column(length = 15, nullable = false)
+    private String city;
+
+    @Column(length = 15, nullable = false)
+    private String country;
+
+    @Column(length = 100, nullable = false)
+    private String streetName;
+
+    @Column(length = 7, nullable = false)
+    private String postalCode;
+
+    @Column(length = 10, nullable = false)
+    private String type;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    private UserDto userDetails;
+}
