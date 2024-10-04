1df329ea0ed80ceeddbf8446882c878784fb598a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-10-04 16:12:09 +0300

    Create test class for user service impl

---
+++ src/test/java/com/develop/app/ws/service/impl/UserServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.develop.app.ws.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getUserByUserId() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
