db0bf70004aa490a5a4c0ff51ae1c05dd68fa16f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-10-15 01:27:31 +0300

    Change kind of the exception on createUser() method

--- src/main/java/com/develop/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/develop/app/ws/service/impl/UserServiceImpl.java
@@ -32,7 +32,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto createUser(UserDto user) {
         if (userRepository.findUserEntityByEmail(user.getEmail()) != null) {
-            throw new RuntimeException("Record already exists");
+            throw new UserServiceException("Record already exists");
         }
 
         for (int i = 0; i < user.getAddresses().size(); i++) {
