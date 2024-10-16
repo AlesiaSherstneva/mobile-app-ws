86c9e17f62b47235a20a4d0d8c2d51dcfebd2d90
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-10-16 21:06:45 +0300

    Create test class for user controller

---
+++ src/test/java/com/develop/app/ws/ui/controller/UserControllerTest.java
@@ -0,0 +1,36 @@
+package com.develop.app.ws.ui.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void getUserAddresses() {
+    }
+
+    @Test
+    void getUserAddress() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
